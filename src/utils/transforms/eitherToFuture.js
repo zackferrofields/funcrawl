de53c9f8ef9233b0213ab48de090e6cbd4bc9d30
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-05 15:16:41 +0000

    eitherToFuture (#11)

---
+++ src/utils/transforms/eitherToFuture.js
@@ -0,0 +1,6 @@
+const { Future, Either } = require('ramda-fantasy');
+
+//:: Either a b ~> Future a b
+const eitherToFuture = Either.either(Future.reject, Future.of);
+
+module.exports = eitherToFuture;
