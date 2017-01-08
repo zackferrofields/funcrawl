72941b161a9bff6a21f6de0c64d6746cdadb3ca1
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-08 10:42:34 +0000

    Update tryEither.js

--- src/utils/tryEither.js
+++ src/utils/tryEither.js
@@ -1,7 +1,7 @@
 const { Either } = require('ramda-fantasy');
 const { compose, tryCatch } = require('ramda');
 
-//:: Function ~> () -> Either a b
+// :: f -> a ~> Either Error f(a)
 const tryEither = f => tryCatch(compose(Either.of, f), Either.Left);
 
 module.exports = tryEither;
